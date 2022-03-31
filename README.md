2a6872d29f485966c4b3d9858b631eb2db297410
BuildTools <unconfigured@null.spigotmc.org>
2022-03-31 10:49:28 +0900

    Prepare Code

---
+++ README.md
@@ -0,0 +1,26 @@
+# RTUBiomeAPI 1.0.0
+[ Minecraft Plugin ] 1.18 ~ 1.18.2 Biome API, Get Custom Biome!
+(Can Only Get String Now)
+
+**RTUBiomeAPI.getInterface(Bukkit.getServer())**
+
+- String **getBiomeName(Location)**
+
+  ex) new:where
+
+  ex) minecraft:birch_forest
+
+- List<String> **getBiomesName()**
+  
+  return list of all the biomes name in server (including custom biome, like "new:where")
+
+- List<String> **getBiomesNameByFabricTag(String)**
+  
+  getBiomesNameByFabric("is_nether")
+  
+  return {"minecraft:nether_wastes", "minecraft:basalt_deltas", "minecraft:soul_sand_valley", "minecraft:crimson_forest", "minecraft:warped_forest"}
+
+
+
+# Gradle&Maven
+maven repo soon maybe...!
--- src/main/java/com/github/ipecter/rtu/languageAPI/RTULanguageAPI.java
+++ src/main/java/com/github/ipecter/rtu/languageAPI/RTULanguageAPI.java
@@ -1,24 +1,7 @@
-package com.github.ipecter.rtu.biomeAPI;
+package com.github.ipecter.rtu.languageAPI;
 
-import com.github.ipecter.rtu.nms.NMSInterface;
-import com.github.ipecter.rtu.nms.NMS_1_18_R1;
-import com.github.ipecter.rtu.nms.NMS_1_18_R2;
-import org.bukkit.Server;
+public final class RTULanguageAPI {
 
-public final class RTUBiomeAPI {
-    private static NMSInterface nmsInterface;
+    
 
-    public static NMSInterface getInterface(Server server) {
-        if (server.getVersion().contains("1.18.2")) {
-            nmsInterface = new NMS_1_18_R2(server);
-        } else if (server.getVersion().contains("1.18.1")) {
-            nmsInterface = new NMS_1_18_R1(server);
-        } else if (server.getVersion().contains("1.18")) {
-            nmsInterface = new NMS_1_18_R1(server);
-        }else{
-            System.err.println("[ RTUBiomeAPI ] Server version is unsupported version!");
-        }
-        return nmsInterface;
-
-    }
 }
